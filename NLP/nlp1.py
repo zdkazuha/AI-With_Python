aea4e1540f900d96b59e4cc041c511ea071759d4
zdkazuha <aartemchelyk@gmail.com>
2026-05-04 20:10:20 +0300

    NPL

---
+++ NLP/nlp1.py
@@ -0,0 +1,56 @@
+from turtle import pos
+
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+import pandas as pd
+
+# Завантаження необхідних мовних ресурсів
+nltk.download("punkt")  # Для токенізації
+nltk.download("punkt_tab")  # Для токенізації речень
+nltk.download("stopwords")  # Стоп-слова
+nltk.download("wordnet")  # Для лемматизації
+nltk.download("omw-1.4")  # WordNet мовні дані
+
+df = pd.read_csv("./assets/reviews_list.csv")
+
+reviews = df['Review Text']
+
+position = 0
+
+data = {
+    "Position": [],
+    "Review Text": []
+    }
+
+for text in reviews:
+    position += 1
+
+    # --- 1. Токенізація ---
+    sentences = sent_tokenize(text)
+
+    words = word_tokenize(text)
+
+    # --- 2. Стоп-слова ---
+    stop_words = set(stopwords.words("english"))
+    filtered_words = [
+        word for word in words if word.lower() not in stop_words and word.isalpha()
+    ]
+
+    # --- 3. Стеммінг ---
+    stemmer = PorterStemmer()
+    stemmed_words = [stemmer.stem(word) for word in filtered_words]
+
+    # --- 4. Лемматизація ---
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word.lower()) for word in filtered_words]
+
+    # --- 5. Збереження ---
+    data["Position"].append(position)
+    data["Review Text"].append(lemmatized_words)
+
+df = pd.DataFrame(data)
+
+# Записуємо в CSV одним методом
+df.to_csv('./assets/reviews_list_new.csv', index=False, encoding='utf-8-sig')
---
+++ NLP/nlp2.py
@@ -0,0 +1,27 @@
+from textblob import TextBlob
+import pandas as pd
+
+df = pd.read_csv("./assets/reviews_list.csv")
+
+reviews = df['Review Text']
+
+count = 0
+
+neutral, good, bad = [], [], []
+
+for text in reviews:
+
+    count += 1
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+
+    if sentiment >= 0.3:
+        good.append(text)
+    elif sentiment <     -0.3:
+        bad.append(text)
+    else:
+        neutral.append(text)
+
+print(f"[Count Neutral] --> {len(neutral)}")
+print(f"[Count Good] -----> {len(good)}")
+print(f"[Count Bad] ------> {len(bad)}")
